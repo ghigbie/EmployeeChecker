6f8a30fa47d94942d3a488a456e4f4d16bf96aa3
George Higbie <gehigbie@gmail.com>
2017-01-27 16:07:01 -0700

    initial commit

---
+++ src/com/georgehigbie/Employee.java
@@ -0,0 +1,10 @@
+package com.georgehigbie;
+
+/**
+ * Created by georgehigbie on 1/27/17.
+ */
+public class Employee {
+
+    private String firstName;
+    private String lastName;
+}
---
+++ src/com/georgehigbie/Intern.java
@@ -0,0 +1,7 @@
+package com.georgehigbie;
+
+/**
+ * Created by georgehigbie on 1/27/17.
+ */
+public class Intern {
+}
