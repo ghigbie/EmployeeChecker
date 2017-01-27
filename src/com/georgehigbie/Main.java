15a6aacbdcf196d43d1c2ce013fb70540e1e1d32
George Higbie <gehigbie@gmail.com>
2017-01-27 16:25:59 -0700

    added prompt for user to choose employee type

--- src/com/georgehigbie/Main.java
+++ src/com/georgehigbie/Main.java
@@ -4,5 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        beginEntry();
+    }
+
+    public static void beginEntry(){
+        System.out.println("Would you like to enter information for an Employee, a Contractor, a Consultant who was a former employee, or an intern?");
+        System.out.println("Please enter \"E\" for employee, \"CONTR\" for contactor, \"CONS\" for consultant, \"I\" for intern.");
+
     }
 }
