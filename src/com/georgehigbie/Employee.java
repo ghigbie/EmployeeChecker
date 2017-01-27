69dfe81079f47c4a61eed1628cd06beb0ee6acd5
George Higbie <gehigbie@gmail.com>
2017-01-27 16:19:25 -0700

    added constructor

--- src/com/georgehigbie/Employee.java
+++ src/com/georgehigbie/Employee.java
@@ -7,4 +7,29 @@ public class Employee {
 
     private String firstName;
     private String lastName;
+    private String dateOfBirth;
+    private String department;
+    private String email;
+    private String workCell;
+    private int age;
+    private int employeeCode;
+
+    public Employee(String firstName, String lastName, String dateOfBirth, String department, String email, String workCell, int age, int employeeCode) {
+        this.firstName = firstName.substring(0, 1).toUpperCase() + firstName.substring(1).toLowerCase();
+        this.lastName = lastName.substring(0,1).toUpperCase() + firstName.substring(1).toLowerCase();
+        this.dateOfBirth = dateOfBirth;
+        this.department = department.substring(0,1).toUpperCase() + department.substring().toLowerCase();
+        this.email = email;
+        this.workCell = workCell;
+        this.age = age;
+        this.employeeCode = employeeCode;
+    }
+
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
 }
