238f2a2df12d55f5e1e14923ad7f0fe17772c8ef
George Higbie <gehigbie@gmail.com>
2017-01-27 17:06:50 -0700

    consultant inhereted from employee class

--- src/com/georgehigbie/ConsultantFormerEmployee.java
+++ src/com/georgehigbie/ConsultantFormerEmployee.java
@@ -14,6 +14,15 @@ public class ConsultantFormerEmployee extends Employee {
     private int age;
     private int employeeCode;
 
-
-
+    public ConsultantFormerEmployee(String firstName, String lastName, String dateOfBirth, String department, String email, String workCell, int age, int employeeCode, String firstName1, String lastName1, String dateOfBirth1, String department1, String email1, String workCell1, int age1, int employeeCode1) {
+        super(firstName, lastName, dateOfBirth, department, email, workCell, age, employeeCode);
+        this.firstName = firstName1;
+        this.lastName = lastName1;
+        this.dateOfBirth = dateOfBirth1;
+        this.department = department1;
+        this.email = email1;
+        this.workCell = workCell1;
+        this.age = age1;
+        this.employeeCode = employeeCode1;
+    }
 }
