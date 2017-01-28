ea6b5674e9e582f729f8cede604134298defffc5
George Higbie <gehigbie@gmail.com>
2017-01-27 17:07:31 -0700

    changed input type to convert to lowercase

--- src/com/georgehigbie/Main.java
+++ src/com/georgehigbie/Main.java
@@ -12,7 +12,7 @@ public class Main {
 
     public static void beginEntry(){
         Scanner scanner = new Scanner(System.in);
-        String employeeType = scanner.next().to
+        String employeeType = scanner.next().toLowerCase();
 
         System.out.println("Would you like to enter information for an Employee, a Contractor, a Consultant who was a former employee, or an intern?");
         System.out.println("Please enter \"E\" for employee, \"CONTR\" for contactor, \"CONS\" for consultant, \"I\" for intern.");
